8d83496c1a84680d50267c0e63b282b54c13c2fc
Tommy1111112 <76492524+Tommy1111112@users.noreply.github.com>
2023-04-16 16:27:12 +0100

    Update E19432B837D23.lua

--- E19432B837D23.lua
+++ E19432B837D23.lua
@@ -1,5 +1,6 @@
 local security = {
     ["160F94A0-6942-4738-B4AE-A64A19E68EC5"] = "8kGUAIZzSgiwJtKbRUflTmwyYlogx7DS", -- gravitytommy
+    ["DA4B9B76-206F-44BC-8316-CE3C9F2BA63E"] = "PPNSVNEgF9EknyYIPtSvl30lHGHZ13OG" -- Desync
 }
 
 return security
